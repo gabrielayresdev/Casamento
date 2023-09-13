2196c55947d5573cc7df8ac63fae091954424b45
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-09-13 19:12:34 -0300

    Feat: React Router DOM added to the project. Page structure created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
